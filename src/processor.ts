0d69c2142d215a0a762e90f2a20e8081c5c0c44a
iankressin <ianguimaraes31@gmail.com>
2025-03-06 13:47:28 -0300

    chore: set finality confirmation for 10 blocks

--- src/processor.ts
+++ src/processor.ts
@@ -7,7 +7,7 @@ export const DRIP_TRADE_CONTRACT_ADDRESS =
 export const processor = new EvmBatchProcessor()
   .setGateway('https://v2.archive.subsquid.io/network/hyperliquid-mainnet')
   .setRpcEndpoint('https://rpc.hyperliquid.xyz/evm')
-  .setFinalityConfirmation(1)
+  .setFinalityConfirmation(10)
   .addLog({
     range: { from: 377_808 },
     address: [DRIP_TRADE_CONTRACT_ADDRESS],
